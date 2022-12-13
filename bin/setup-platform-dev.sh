b739365db12d88e98b1c93e6254d2df864c46595
Em Jones <emjonesdev@gmail.com>
2022-12-13 13:31:17 -0600

    platform(init): skip installed dependencies

--- bin/setup-platform-dev.sh
+++ bin/setup-platform-dev.sh
@@ -2,12 +2,18 @@
 helm repo add traefik https://traefik.github.io/charts
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 
+
 ## Apple intel chip setup
 if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Installing Apple Intel dependencies"
     (curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.27.0/pack-v0.27.0-macos.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
 else 
-    echo "Installing Linux x86_64 dependencies"
-    (curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.27.0/pack-v0.27.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
+    echo "Installing Linux x86_64 dependencies";
+    if command -v pack &> /dev/null; then \
+        echo "'pack' already installed"
+    else
+        (curl -sSL "https://github.com/buildpacks/pack/releases/download/v0.27.0/pack-v0.27.0-linux.tgz" | sudo tar -C /usr/local/bin/ --no-same-owner -xzv pack)
+    	exit; \
+    fi;
 fi
 
