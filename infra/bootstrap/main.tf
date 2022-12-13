fb48d9858f4d31b058592d6aef2d2900f993ab86
Tom George <tom.george@procore.com>
2022-12-13 12:00:23 -0600

    run terraform fmt

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -16,11 +16,11 @@ terraform {
   }
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
+      source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
   }
-} 
+}
 
 variable "do_token" {
   default = ""
